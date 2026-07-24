aa784735121c28f4658c79d4519b45f543ffa790
sb2bg <sullivanbognar@gmail.com>
2026-07-24 18:42:20 -0500

    test: retire resolved arena overflow fixtures

--- tests/compile_fail/stress_product_simplification_overflow.zig
+++
@@ -1,9 +0,0 @@
-const bombelli = @import("bombelli");
-
-comptime {
-    const product = bombelli.expr(
-        "(x + 1) * (x + 2) * (x + 3) * (x + 4) * (x + 5) * (x + 6) * " ++
-            "(x + 7) * (x + 8) * (x + 9) * (x + 10) * (x + 11) * (x + 12)",
-    );
-    _ = product.diff(.x).simplify();
-}
